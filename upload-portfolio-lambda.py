0a7855428895858aade4738586e5326865f69000
Karl Mathias Moberg <karlmathias@gmail.com>
2020-07-09 02:15:22 +0200

    Added Lambda python uploader to S3

---
+++ upload-portfolio-lambda.py
@@ -0,0 +1,17 @@
+import boto3
+import io
+import zipfile
+
+s3 = boto3.resource('s3')
+
+portfolio_bucket = s3.Bucket('portfolio.kmoberg.cloud')
+build_bucket = s3.Bucket('portfoliobuild.kmoberg.cloud')
+
+portfolio_zip = io.BytesIO()
+build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+with zipfile.ZipFile(portfolio_zip) as myzip:
+    for nm in myzip.namelist():
+        obj = myzip.open(nm)
+        portfolio_bucket.upload_fileobj(obj, nm)
+        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
